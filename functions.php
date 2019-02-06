ae57458078adfc2566fec758f1ce6cdc53720a4d
kalpesh Agrawal <kalpeshagrawal2@gmail.com>
2019-02-05 19:36:26 -0500

    Theme setup

---
+++ functions.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * sample2019 functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package sample2019
+ */
+require get_template_directory() .'/inc/setup.php';
+/**
+ * Enqueue scripts and styles.
+ */
+function sample2019_scripts() {
+	wp_enqueue_style( 'sample2019-style', get_stylesheet_uri() );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'sample2019_scripts' );
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+if ( defined( 'JETPACK__VERSION' ) ) {
+	require get_template_directory() . '/inc/jetpack.php';
+}
+
